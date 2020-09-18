773a46c2fbdda446d087e0cc09f8ffb9abafe450
Thami A. Vicente <thamiavicente@gmail.com>
2020-09-18 18:50:04 -0300

    fixing contact page

--- forms/contact.php
+++ forms/contact.php
@@ -1,37 +1,23 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'thamiavicente@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  if (isset($_POST['email']) && !empty($_POST['email'])) {
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $name = addslashes($_POST['name'])
+  $email = addslashes($_POST['email'])
+  $formSubject = addslashes($_POST['subject'])
+  $message = addslashes($_POST['message'])
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  $to = "thamiavicente@gmail.com";
+  $subject = $formSubject;
+  $body = "Name: ".$nome. "\r\n".
+          "Email: ".$email. "\r\n".
+          "Messagem: ".$message;
+  $header = "From: contato@thamiavicente.com"."\r\n".
+            "Reply-To: ".$email."\r\n".
+            "X=Mailer:PHP/".phpversion();
+  
+  
+  }
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
